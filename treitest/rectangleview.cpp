fe8d177116a5b53e6e0b137df8ce2eca163a7cd5
aleksandr12314241 <eastoutcity@mail.ru>
2025-02-04 23:52:50 +0300

    Add rectangle view class

---
+++ treitest/rectangleview.cpp
@@ -0,0 +1,5 @@
+#include "rectangleview.h"
+
+RectangleView::RectangleView() {
+    qDebug() << "hello !";
+}
---
+++ treitest/rectangleview.h
@@ -0,0 +1,12 @@
+#ifndef RECTANGLEVIEW_H
+#define RECTANGLEVIEW_H
+
+#include <QDebug>
+
+class RectangleView
+{
+public:
+    RectangleView();
+};
+
+#endif // RECTANGLEVIEW_H
